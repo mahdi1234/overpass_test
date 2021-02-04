44c6389a22706c15629be60dd5df1dbad0f500f2
mahdi1234 <mahdi1234@centrum.cz>
2021-02-04 20:33:08 +0000

    Remove timestamps from jsons

--- run.sh
+++ run.sh
@@ -12,3 +12,4 @@ wget -O vegetarian_yes.json 'https://overpass-api.de/api/interpreter?data=%5Bout
 sleep 300
 # diet:vegetarian=only IN 'Jihomoravský kraj'
 wget -O vegetarian_only.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea(3600442311)-%3E.searchArea%3B(node%5B%22diet%3Avegetarian%22%3D%22only%22%5D(area.searchArea)%3Bway%5B%22diet%3Avegetarian%22%3D%22only%22%5D(area.searchArea)%3Brelation%5B%22diet%3Avegetarian%22%3D%22only%22%5D(area.searchArea)%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B'
+sed -i '/.*timestamp_.*/d' *.json
